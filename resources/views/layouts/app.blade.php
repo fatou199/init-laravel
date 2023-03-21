dab72fcdc48bda9488591d1cae8d3ec3abb2d59e
fatou199 <65873642+fatou199@users.noreply.github.com>
2023-03-21 21:07:45 +0100

    changement du title de la page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous" defer></script>
     <link rel="stylesheet" href={{ asset('css/style.css') }}>
-    <title>Document</title>
+    <title>Initiation laravel 9</title>
 </head>
 <body>
     {{-- récupérer le contenu de la page enfant vers une page maître --}}
